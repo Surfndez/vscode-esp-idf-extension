fd30b7036f7d2e737adb07b07334194fb20c7beb
Brian Ignacio <brian.ignacio@espressif.com>
2021-08-16 18:16:17 +0800

    ui test as separated job
    add clone repo to ui test job
    
    output log ui entrypoint
    
    output ui test log
    
    fix test log output
    
    fix open folder and timeouts
    
    quote test files regex
    
    use mocha config
    
    test ubuntu fixes

--- .github/actions/idf/ui-entrypoint.sh
+++ .github/actions/idf/ui-entrypoint.sh
@@ -16,7 +16,9 @@ export PY_VERSION=$( echo "$a" | echo $(python --version) | sed -nre 's/^[^0-9]*
 export PIP_VERSION=$( echo "$a" | echo $(python -m pip --version) | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p')
 export PY_PKGS=$(python -m pip list --format json)
 
+rm -rf out
 yarn
 yarn lint
 Xvfb -ac :99 -screen 0 1920x1080x16 & sleep 2 & yarn ui-test
-echo ::set-output name=uiresult::$(cat ./out/results/ui-test-results.xml)
+ls out/ui-test
+echo ::set-output name=uiresult::$(cat test-resources/test.log)
